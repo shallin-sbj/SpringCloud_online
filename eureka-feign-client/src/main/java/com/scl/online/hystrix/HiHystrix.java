6e69cd01615edf62e6c946c18c406c142b334276
User <sucuilin27@sina.com>
2019-09-11 00:18:33 +0800

    init

---
+++ eureka-feign-client/src/main/java/com/scl/online/hystrix/HiHystrix.java
@@ -0,0 +1,13 @@
+package com.scl.online.hystrix;
+
+import com.scl.online.feign.EurekaClientFeign;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HiHystrix implements EurekaClientFeign {
+
+    @Override
+    public String sayHiFromClientEureka(String name) {
+        return "hi," + name + ",sorry,error! this is feign + hystrix !";
+    }
+}
