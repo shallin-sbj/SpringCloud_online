728d17749b96b871230ce9598ca838a9b40b0a84
User <sucuilin27@sina.com>
2019-09-22 20:38:48 +0800

    init

---
+++ user-service/src/main/java/com/scl/online/userservice/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.scl.online.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "I'm forezp";
+    }
+}
