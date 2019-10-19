af282d7d86f320530b1476aeb522a2490893ff8c
User <sucuilin27@sina.com>
2019-10-19 16:23:41 +0800

    init

--- admin-server/src/main/java/com/scl/online/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/scl/online/adminserver/AdminServerApplication.java
@@ -3,14 +3,24 @@ package com.scl.online.adminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * to see https://codecentric.github.io/spring-boot-admin/2.0.0/
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@Configuration
 @EnableAdminServer
 public class AdminServerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AdminServerApplication.class, args);
     }
 
