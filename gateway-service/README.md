03689502afca0019801d390545d137208a199ff5
User <sucuilin27@sina.com>
2019-10-19 16:27:11 +0800

    init

--- gateway-service/README.md
+++ gateway-service/README.md
@@ -10,3 +10,12 @@ getway-service  -》user-service
 通过zipkin-server  可以发现服务。
 
 问题：链路追踪时，需要先调用服务提供者，然后在通过链路调用服务提供者，才能够正常显示。
+
+
+```$xslt
+spring.application.name        配置的名称会在 Spring Boot Admin 的 UI 界面中显示出来。
+spring.boot.admin.client.url   配置的是我们 Spring Boot Admin 服务端的地址。
+management.endpoints.web.exposure.include   开启 Spring Boot Actuator 的全部监控。
+management.endpoint.health.show-details     开启 Spring Boot Actuator 监控的应用的详细的应用健康信息.
+```
+
--- gateway-service/src/main/java/com/scl/online/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/scl/online/gatewayservice/GatewayServiceApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
